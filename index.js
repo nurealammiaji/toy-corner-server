6f85952a246d34074ca2fb25159478aae30e63f8
nurealammiaji <nurealam.miaji@gmail.com>
2023-11-12 00:55:59 +0600

    products getting api added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -13,14 +14,14 @@ const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
 
 app.get("/", (req, res) => {
-    res.send("Toy Corner Server");
+  res.send("Toy Corner Server");
 })
 
 app.listen(port, () => {
-    console.log(`Toy Corner Server is running on port: ${port}`);
+  console.log(`Toy Corner Server is running on port: ${port}`);
 })
 
-
+// MongoDB Driver
 const uri = `mongodb+srv://${username}:${password}@cluster0.31s3qjy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,6 +37,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
+
+    const productsCollection = client.db("toyCorner").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
