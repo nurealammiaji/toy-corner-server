008818403597197de925364212fb28496bdf1706
nurealammiaji <nurealam.miaji@gmail.com>
2023-11-13 02:51:11 +0600

    single data finding  by an id added

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
       res.send(result);
     })
 
+    app.get("/products/:id", async(req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
